a86ffc7af49475451e6884f432535039fdc0a99b
Nauryzbai <nauzik@mail.ru>
2019-03-27 13:01:44 +0600

    Танки

---
+++ Day7_Tanks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyClassLib;
+//using MyClassLib.WorlOfTanks;
+
+namespace Day7_Tanks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Tank tank = new Tank("T-34", rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100));
+            Tank secondTank = new Tank("Panthera", rand.Next(0, 100), rand.Next(0, 100), rand.Next(0, 100));
+
+
+            Console.WriteLine("Name1: " + tank.Name_Tanks);
+            Console.WriteLine("Уровень защиты первого танка: " + tank.Armor_Level);
+            Console.WriteLine("Уровень боезапаса первого танка: " + tank.Level_Ammunition_load);
+            Console.WriteLine("Уровень манёврености первого танка: " + tank.Level_Of_Agility);
+
+
+            Console.WriteLine("Name2: " + secondTank.Name_Tanks);
+            Console.WriteLine("Уровень защиты второго танка: " + secondTank.Armor_Level);
+            Console.WriteLine("Уровень боезапаса второго танка: " + secondTank.Level_Ammunition_load);
+            Console.WriteLine("Уровень манёврености второго танка: " + secondTank.Level_Of_Agility);
+
+            Tank winTank = tank * secondTank;
+            Console.WriteLine("Победитель!");
+            Console.WriteLine("Name: " + winTank.Name_Tanks);
+            Console.WriteLine("Уровень защиты танка: " + winTank.Armor_Level);
+            Console.WriteLine("Уровень боезапаса танка: " + winTank.Level_Ammunition_load);
+            Console.WriteLine("Уровень манёврености танка: " + winTank.Level_Of_Agility);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MyClassLib/Tank.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLib
+{
+    public class Tank
+    {
+        public string Name_Tanks { get; set; }
+        public int Level_Ammunition_load { get; set; }
+        public int Armor_Level { get; set; }
+        public int Level_Of_Agility { get; set; }
+
+        public Tank(string name_Tanks, int level_Ammunition_load, int armor_Level, int level_Of_Agility)
+        {
+            Name_Tanks = name_Tanks;
+            Level_Ammunition_load = level_Ammunition_load;
+            Armor_Level = armor_Level;
+            Level_Of_Agility = level_Of_Agility;
+        }
+
+        public static Tank operator *(Tank firstTank, Tank secondTank)
+        {
+            if (firstTank.Armor_Level > secondTank.Armor_Level &&
+                firstTank.Level_Ammunition_load > secondTank.Level_Ammunition_load
+                || firstTank.Armor_Level > secondTank.Armor_Level &&
+                firstTank.Level_Of_Agility > secondTank.Level_Of_Agility
+                || firstTank.Level_Ammunition_load > secondTank.Level_Ammunition_load &&
+                firstTank.Level_Of_Agility > secondTank.Level_Of_Agility)
+            {
+                return new Tank(firstTank.Name_Tanks, firstTank.Armor_Level, firstTank.Level_Ammunition_load, firstTank.Level_Of_Agility);
+            }
+            else
+            {
+                return new Tank(secondTank.Name_Tanks, secondTank.Armor_Level, secondTank.Level_Ammunition_load, secondTank.Level_Of_Agility);
+            }
+        }
+    }
+}
